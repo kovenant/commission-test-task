73e3b461cb1070ce763bab6d51feb93130738ca4
Anton <kovenant.rus@gmail.com>
2021-08-03 17:06:19 +0300

    fix: add build method to UserFactoryInterface

--- src/Factory/User/UserFactoryInterface.php
+++ src/Factory/User/UserFactoryInterface.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Factory\User;
 
+use App\DTO\User;
+
 interface UserFactoryInterface
 {
+    public function build(string $userId, string $userType): User;
 }
